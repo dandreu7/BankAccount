0d6a4998a20517292dc9cbdb1f10c928c6826f76
alliecostner27 <alliecostner3@gmail.com>
2024-09-18 17:47:31 -0400

    Update savings method

--- savings.py
+++ savings.py
@@ -1,12 +1,17 @@
+from BankAccount import BankAccount
 
-'''class savings(BankAccount):
-    def __init__(self, name, balance, min_balance):
-        super().__init__(name, balance, min_balance)
-
-    def add_interest(self, interest_rate):
-        interest = self.balance * interest_rate
-        self.balance += interest
-        return interest'''
+class savings(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self._interest_rate = interest_rate
+        
+        
+    def apply_interest(self):
+        interest = self.current_balance * (self._interest_rate / 100)
+        self.current_balance += interest
+        print(f"Interest applied at {self._interest_rate}%: {interest}")
+        print(f"New Balance after interest: {self.current_balance}\n")
+        return self.current_balance
 
 
 
